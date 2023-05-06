91da5307358780ca9186a21ef08eb9a3a46402c9
Farah <farah.challakhi@stud.fh-campuswien.ac.at>
2023-05-06 16:47:26 +0200

    Worked on the projectCard

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,10 +1,13 @@
 import { ProjectCard } from '@/components/ui/ProjectCard'
+import { projectList } from '@/data'
 import Image from 'next/image'
 
 export default function Home() {
   return (
+
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ProjectCard />
+          <div className="grid grid-cols-3 gap-4">{projectList.map((item) => <ProjectCard title={item.title} projectId={item.projectId} detail={item.detail} nameProjectOwner={item.nameProjectOwner} technology={item.technology} />
+)}</div>
     </main>
   )
 }
